37f391889ee50273a571faac01eb55ff2b9bf155
JayanikaGithub <AR96588@fhss.sjp.ac.lk>
2022-07-11 10:13:22 +0530

    create needed files under model folder

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,8 +5,8 @@ router.get("/usertest",(req,res)=>{
 });
 
 
-router.post("userposttest", (req,res)=>{
+router.post("/userposttest", (req,res)=>{
     const username = req.body.username;
-    console.log(username);
+    res.send("your username is:"+ username)
 });
 module.exports = router;
